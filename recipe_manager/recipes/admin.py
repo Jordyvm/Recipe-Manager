b4b06089df88366563137cc556717be30eadbf1e
Jordy <jordy>
2021-04-21 23:37:15 +0200

    New database structure - Measures - Ingredient amount

--- recipe_manager/recipes/admin.py
+++ recipe_manager/recipes/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 
-from .models import ingredient, recipe, tag
+from .models import ingredient, recipe, tag, ingredientAmount, measure
 
 admin.site.register(ingredient)
-admin.site.register(recipe)
+admin.site.register(measure)
+# admin.site.register(recipe)
 admin.site.register(tag)
+
+# @admin.register(ingredientAmount)
+class ingredientAmountInline(admin.TabularInline):
+    model = ingredientAmount
+    extra = 1
+
+# @admin.register(ingredient)
+# class ingredientAdmin(admin.ModelAdmin):
+#     inlines = (ingredientAmountInline,)
+
+@admin.register(recipe)
+class recipeAdmin(admin.ModelAdmin):
+    inlines = (ingredientAmountInline,)
+
--- recipe_manager/recipes/models.py
+++ recipe_manager/recipes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ingredient(models.Model):
-    name = models.CharField(max_length = 50, unique = True)
+    name = models.CharField(max_length = 50, unique=True)
 
     def __str__(self):
         return self.name
@@ -10,24 +10,42 @@ class ingredient(models.Model):
         ordering = ['name']
 
 class tag(models.Model):
-    tag = models.CharField(max_length = 50, unique = True)
+    name = models.CharField(max_length = 50, unique=True)
 
     def __str__(self):
         return self.name
     
     class Meta:
-        ordering = ['tag']
+        ordering = ['name']
 
 class recipe(models.Model):
-    title = models.CharField(max_length = 50, unique = True)
-    ingredients = models.ManyToManyField(ingredient, blank=True)
+    title = models.CharField(max_length = 50, unique=True)
+    servings = models.PositiveSmallIntegerField()
+    ingredients = models.ManyToManyField(ingredient, blank=True, through='ingredientAmount')
+    description = models.TextField(max_length = 500)
     tags = models.ManyToManyField(tag, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
     
     class Meta:
         ordering = ['title']
+
+class measure(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+class ingredientAmount(models.Model):
+    amount = models.FloatField()
+    measure = models.ForeignKey(measure,on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(recipe, on_delete=models.CASCADE)
+
 
 
 
